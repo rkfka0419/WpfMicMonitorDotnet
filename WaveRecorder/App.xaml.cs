72537c4ece88fa4fde228f93a71bba741d987a26
Garam <rkfka0419@gmail.com>
2025-07-13 01:25:43 +0900

    Add WaveRecorder Project, start from Random Data to Plotting WPF

---
+++ WaveRecorder/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows;
+
+namespace WaveRecorder;
+public partial class App : Application
+{
+}
---
+++ WaveRecorder/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ScottPlot;
+using System.Windows;
+namespace WaveRecorder;
+
+public partial class MainWindow : Window
+{
+	Timer timer;
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		timer = new Timer(UpdateChart, null, 0, 1000);
+	}
+
+	Random rand = new Random();
+	public double[] GetRandomData()
+	{
+		double[] data = new double[100];
+		for (int i = 0; i < data.Length; i++)
+		{
+			data[i] = rand.NextDouble() * 100;
+		}
+		return data;
+	}
+
+	public void UpdateChart(object? o)
+	{
+		micPlot.Plot.Clear();
+		micPlot.Plot.Add.Signal(GetRandomData());
+		micPlot.Refresh();
+	}
+}
