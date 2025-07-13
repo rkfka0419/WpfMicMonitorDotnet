74c79a1f0464ebf20b028c15671521f8633431e5
Garam <rkfka0419@gmail.com>
2025-07-13 21:48:18 +0900

    Change Plotting Data Timer to Dispathcer Timer

--- WaveRecorder/MainWindow.xaml.cs
+++ WaveRecorder/MainWindow.xaml.cs
@@ -4,29 +4,55 @@ namespace WaveRecorder;
 
 public partial class MainWindow : Window
 {
-	Timer timer;
+	System.Timers.Timer SystemTimer = new() { Interval = 10 };
+	private readonly System.Windows.Threading.DispatcherTimer DispatcherTimer = new() { Interval = TimeSpan.FromMilliseconds(10) };
+
+	private readonly List<double> Xs = [];
+	private readonly List<double> Ys = [];
 	public MainWindow()
 	{
 		InitializeComponent();
 
-		timer = new Timer(UpdateChart, null, 0, 1000);
-	}
+		// pre-populate lists with valid data
+		ChangeDataLength();
 
-	Random rand = new Random();
-	public double[] GetRandomData()
-	{
-		double[] data = new double[100];
-		for (int i = 0; i < data.Length; i++)
+		// add the scatter plot
+		micPlot.Plot.Add.ScatterLine(Xs, Ys);
+
+		SystemTimer.Elapsed += (s, e) =>
 		{
-			data[i] = rand.NextDouble() * 100;
-		}
-		return data;
+			// Changing data length will throw an exception if it occurs mid-render.
+			// Operations performed while the sync object will occur outside renders.
+			lock (micPlot.Plot.Sync)
+			{
+				ChangeDataLength();
+			}
+			micPlot.Refresh();
+		};
+
+		DispatcherTimer.Tick += (s, e) =>
+		{
+			// Locking the sync object does not seem to be required
+			// when data is changed using the dispatcher timer in WPF apps
+			ChangeDataLength();
+			micPlot.Refresh();
+		};
+
 	}
 
-	public void UpdateChart(object? o)
+
+	private void ChangeDataLength(int minLength = 10_000, int maxLength = 20_000)
 	{
-		micPlot.Plot.Clear();
-		micPlot.Plot.Add.Signal(GetRandomData());
-		micPlot.Refresh();
+		int newLength = Random.Shared.Next(minLength, maxLength);
+		Xs.Clear();
+		Ys.Clear();
+		Xs.AddRange(Generate.Consecutive(newLength));
+		Ys.AddRange(Generate.RandomWalk(newLength));
+		micPlot.Plot.Axes.AutoScale(true);
+	}
+
+	private void micPlot_Loaded(object sender, RoutedEventArgs e)
+	{
+		DispatcherTimer.Start();
 	}
 }
